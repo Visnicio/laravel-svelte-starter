73fb7cecef3b2560ae7bbfffdd9c2d999fa63d5f
Visnicio <vinicius.antonio0712@gmail.com>
2023-05-13 22:49:27 -0300

    first commit

---
+++ resources/js/Pages/Welcome.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let counter;
+
+  function increment() {
+    counter ++;
+  }
+</script>
+
+<button on:click={increment}>
+  Counter: {counter}
+</button>
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+
+        svelte({}),
+    ],
+});
